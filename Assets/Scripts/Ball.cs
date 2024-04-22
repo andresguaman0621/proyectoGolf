7524b13439c22462918b471f62ba9b09035b2372
Andres Alejandro Guaman Taco <andres.guaman.taco@udla.edu.ec>
2024-04-22 16:00:30 -0500

    second commit
    uso de probuilder para obstaculos, calculo de angulo, simulacion palo de golf, movimiento camara

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Net.NetworkInformation;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    private GameObject mainCam;
-    private Vector3 camZoomOffset;
+    private Camera mainCam;
+    private GameObject putter;
+    
     private Rigidbody rb;
+    private Vector3 originPosition;
     private Vector3 oldPosition;
 
     public float AbsHorizonMagnitude;
@@ -15,10 +20,11 @@ public class Ball : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mainCam = GameObject.Find("Main Camera");
-        camZoomOffset = transform.position - mainCam.transform.position;
+        mainCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        putter = GameObject.Find("Putter");
         rb=GetComponent<Rigidbody>();
-        oldPosition = transform.position;   
+        originPosition = transform.position;    
+        oldPosition = originPosition;   
     }
 
     // Update is called once per frame
@@ -48,8 +54,16 @@ public class Ball : MonoBehaviour
             rb.AddForce(new Vector3(0, 0, -Speed));
         }
 
+        if (Input.GetKeyDown(KeyCode.Space) || transform.position.y < -10)
+        {
+            transform.position = originPosition;
+            rb.velocity = new Vector3();
+            rb.angularVelocity = new Vector3();
+        }
         //Slow down    
         AbsHorizonMagnitude = Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.z);
+
+        UpdatePutterPosition();
 
         UpdateCamera();
 
@@ -80,5 +94,34 @@ public class Ball : MonoBehaviour
         //Follow camera
         mainCam.transform.Translate(transform.position - oldPosition, Space.World);
         //mainCam.transform.position = transform.position - camZoomOffset;
+    }
+
+    private void UpdatePutterPosition()
+    {
+        float putterDistanceAway = 0.7f;
+
+        //Figure out the angle from the ball to the cursor
+        var ray = mainCam.ScreenPointToRay(Input.mousePosition);
+        var putterLayer = 1 << LayerMask.NameToLayer("PutterCollider");
+
+        if (Physics.Raycast(ray.origin, ray.direction, out var hitInfo, 10, putterLayer)
+            && Vector3.Distance(transform.position, hitInfo.point) > 0.3f)
+        {
+            Debug.DrawLine(ray.origin, hitInfo.point);
+
+            float deltaY = hitInfo.point.y - transform.position.y;
+            float deltaX = hitInfo.point.x - transform.position.x;
+            float angle = Mathf.Atan2(deltaY, deltaX) * 180 / Mathf.PI;
+            angle *= hitInfo.point.z < transform.position.z ? -1 : 1;
+
+            Debug.Log(angle);
+            //Move putter to distance from ball based on found angle
+            var newPoint = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), 0, Mathf.Sin(angle * Mathf.Deg2Rad));
+            putter.transform.position = transform.position + (newPoint * putterDistanceAway);
+            
+        }
+
+
+
     }
 }
